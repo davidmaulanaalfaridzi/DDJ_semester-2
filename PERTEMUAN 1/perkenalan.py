932f960f5f46a5f976386a397434cdec8c22e8a4
David Maulana Alfaridzi <140993760+davidmaulanaalfaridzi@users.noreply.github.com>
2024-01-15 09:45:16 +0700

    Add files via upload

---
+++ PERTEMUAN 1/perkenalan.py
@@ -0,0 +1,6 @@
+nama = "Davi Maulana Al-faridzi"
+kelas = "TKJ X A"
+jurusan = "Tehnik Komputer dan Jaringan"
+
+print("Perkenalkan nama saya , nama")
+print("saya kelas ", kelas, "dari jurusan" jurusan)
